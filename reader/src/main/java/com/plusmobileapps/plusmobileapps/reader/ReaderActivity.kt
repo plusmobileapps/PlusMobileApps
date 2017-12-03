77f2b22bbc8969261c0feedc4eaed4b4ce96ae96
Andrew Steinmetz <arsteinm@asu.edu>
2017-12-02 17:33:38 -0800

    refined the nav bar

--- reader/src/main/java/com/plusmobileapps/plusmobileapps/reader/ReaderActivity.kt
+++ reader/src/main/java/com/plusmobileapps/plusmobileapps/reader/ReaderActivity.kt
@@ -58,23 +58,8 @@ class ReaderActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_instant -> {
                 // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
             }
         }
 
